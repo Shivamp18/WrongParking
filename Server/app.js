d5f9947294eb81a33cb59ffa4385825797ae6837
Shivamp18 <s46hivam17@gmail.com>
2026-01-16 21:03:02 +0530

    Deployment

--- Server/app.js
+++ Server/app.js
@@ -10,7 +10,13 @@ dotenv.config();
 const app = express();
 
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "https://parking.netlify.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json());
 
 
